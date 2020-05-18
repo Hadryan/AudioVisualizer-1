a84a983c7f23c5089392429add272130fa3916ef
Mikael Escolin <mikaelescolin@gmail.com>
2020-05-18 18:41:20 +0100

    Add particle system

---
+++ src/Visuals/particles/Particle.java
@@ -0,0 +1,87 @@
+package Visuals.particles;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Particle {
+  private PVector coordinates;
+  private PVector acceleration;
+
+  private float life;
+  private float circleWidth;
+
+  private int time = 0;
+  private float amplitude;
+
+  protected final PApplet processing;
+
+  public Particle(PApplet processing, float x, float y, float acceleration) {
+    coordinates = new PVector(x, y);
+    this.acceleration = new PVector(0, -acceleration);
+    this.processing = processing;
+    life = processing.random(30, 300);
+    circleWidth = processing.random(1, 10);
+    amplitude = processing.random(0, 1);
+  }
+
+  public float getX() {
+    return coordinates.x;
+  }
+
+  public float getY() {
+    return coordinates.y;
+  }
+
+  public PVector getCoordinates() {
+    return coordinates;
+  }
+
+  public float getAcceleration() {
+    PApplet.println("Accel is " + acceleration.y);
+    return acceleration.y;
+  }
+
+  public void setAcceleration(double newAcceleration) {
+    if (newAcceleration < 0) {
+      newAcceleration *= -1;
+    }
+    acceleration.set(0, (float) -newAcceleration);
+  }
+
+  public void increaseAcceleration(float increment) {
+    acceleration.add(0, increment);
+  }
+
+  public void decreaseAcceleration(float decrement) {
+    acceleration.sub(0, decrement);
+  }
+
+  public boolean isDead() {
+    return life <= 0;
+  }
+
+  public void update() {
+    life--;
+    if (processing.frameCount % 60 == 0) {
+      amplitude = PApplet.lerp(amplitude, processing.random(0, 1), .4f);
+    }
+
+    time++;
+    move();
+    display();
+  }
+
+  public void move() {
+    coordinates.add(acceleration);
+
+    float xPos = amplitude * PApplet.sin(2 * PApplet.PI * .1f * time * PApplet.radians(3));
+
+    coordinates.add(xPos, 0);
+  }
+
+  public void display() {
+    processing.noStroke();
+    processing.fill(255, 0, PApplet.map(life, 50, 500, 0, 255));
+    processing.ellipse(coordinates.x, coordinates.y, circleWidth, circleWidth);
+  }
+}
---
+++ src/Visuals/particles/ParticlesSystem.java
@@ -0,0 +1,79 @@
+package Visuals.particles;
+
+import processing.core.PApplet;
+
+import Controller.AudioController;
+
+import java.util.ArrayList;
+
+public class ParticlesSystem extends AudioController {
+  private ArrayList<Particle> particles;
+
+  private float circleAcceleration = 0;
+  private float lerpedCircleAcceleration = 0;
+
+  public void settings() {
+    size(1024, 700);
+  }
+
+  public void setup() {
+    colorMode(HSB);
+
+    setFrameSize(1024);
+    startMinim();
+    loadAudio("All My Friends.mp3");
+    playAudio();
+
+    particles = new ArrayList<Particle>();
+
+    for (int i = 0; i < 500; i++) {
+      addParticle();
+    }
+  }
+
+  public void addParticle() {
+    float xPos = random(0, width);
+    particles.add(new Particle(this, xPos, height + 10, random(0.2f, 1)));
+  }
+
+  public void keyPressed() {
+    if (key == ' ') {
+      playAudio();
+    }
+  }
+
+  public void draw() {
+    background(0);
+
+    float sum = 0;
+
+    for (int i = 0; i < getAudioPlayer().bufferSize(); i++) {
+      sum += abs(getAudioBuffer().get(i));
+    }
+
+    circleAcceleration = sum / getAudioPlayer().bufferSize();
+    circleAcceleration *= 10000;
+
+    calculateAverageAmplitude();
+
+    for (int i = particles.size() - 1; i >= 0; i--) {
+      double temp = particles.get(i).getAcceleration();
+      Particle currParticle = particles.get(i);
+
+      float mappedCircleAcceleration = map(circleAcceleration, 0f, 200, .2f, 1.5f);
+      float lerpedCircleAcceleration =
+          lerp((float) temp, (float) temp + mappedCircleAcceleration, .3f);
+
+
+      println(-(getAmplitude() * 10));
+      currParticle.increaseAcceleration(-(getAmplitude() * 10));
+      currParticle.update();
+      currParticle.setAcceleration(temp);
+
+      if (currParticle.isDead() == true) {
+        particles.remove(i);
+        addParticle();
+      }
+    }
+  }
+}
