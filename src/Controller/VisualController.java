84d9d632aabf7d70caa36a28bd4a2b8282cc67f5
Mikael Escolin <mikaelescolin@gmail.com>
2020-05-18 21:16:22 +0100

    Revert "Revert "no""
    This reverts commit 17e2a1df2f3a831d785431ea10dba5c066f3d1d5.

---
+++ src/Controller/VisualController.java
@@ -0,0 +1,37 @@
+package Controller;
+
+import Visuals.background.Background;
+
+public class VisualController extends AudioController {
+  private float halfWidth, halfHeight;
+  private Background background;
+
+  public void settings() {
+    size(1024, 700);
+  }
+
+  public void setup() {
+    startMinim();
+    loadAudio("All My Friends.mp3");
+    background = new Background(this);
+  }
+
+  public void keyPressed() {
+    if (key == ' ') {
+      getAudioPlayer().cue(0);
+      getAudioPlayer().play();
+    }
+  }
+
+  public float getHalfWidth() {
+    return halfWidth;
+  }
+
+  public float getHalfHeight() {
+    return halfHeight;
+  }
+
+  public void draw() {
+    background.display();
+  }
+}
